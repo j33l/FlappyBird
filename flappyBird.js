28ccb05e040a7b1ed0800c2272459e596bcd27be
Jeel_Z <45571080+jeelpatel1612@users.noreply.github.com>
2020-02-16 23:59:51 +0530

    made little changes in original code

---
+++ flappyBird.js
@@ -0,0 +1,117 @@
+// Flappy Bird game using JavaScript
+
+// display canvas
+var canvas = document.getElementById("myCanvas");
+var contex = canvas.getContext("2d");
+
+// loading images
+var bird = new Image();
+var backgroundImage = new Image();
+var frontground = new Image();
+var topPipe = new Image();
+var bottomPipe = new Image();
+
+bird.src = "assets/images/bird.png";
+backgroundImage.src = "assets/images/backgroundImage.png";
+frontground.src = "assets/images/frontground.png";
+topPipe.src = "assets/images/topPipe.png";
+bottomPipe.src = "assets/images/bottomPipe.png";
+
+// audio files
+var birdFly = new Audio();
+var scoreIncrement = new Audio();
+
+birdFly.src = "assets/sounds/birdFly.mp3";
+scoreIncrement.src = "assets/sounds/scoreIncrement.mp3";
+
+
+// game physics variables
+var pipesGap = 100;
+var constant;
+
+var xBird = 10;
+var yBird = 150;
+
+var gravity = 1.3;
+
+var score = 0;
+
+// on key down
+document.addEventListener("keydown", moveUp);
+
+function moveUp() {
+    yBird -= 25;
+    birdFly.play();
+}
+
+// pipe coordinates
+var pipes = [];
+
+pipes[0] = { // initial pipes
+    x : canvas.width,
+    y : 0
+};
+
+// draw images
+function draw() {
+
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
+    
+    // drawing backgroung image
+    contex.drawImage(backgroundImage, 0, 0); // imageName, x position, y position, widgth, height
+    
+    // drawing all available poles
+    for(var i = 0; i < pipes.length; i++) {
+        
+        constant = topPipe.height + pipesGap;
+        contex.drawImage(topPipe, pipes[i].x, pipes[i].y);
+        contex.drawImage(bottomPipe, pipes[i].x, pipes[i].y+constant);
+
+        pipes[i].x--; // moving all pipes left
+        
+        // adding new pipeCouples to the array when oldest reachec almost middle
+        if( pipes[i].x == canvas.width - 188) {
+            pipes.push({
+                x : canvas.width,
+                y : Math.floor( Math.random() * topPipe.height ) - topPipe.height // new pole with random y position
+            });
+
+            // removing old pipes
+            if( pipes.length > 3 ) {
+                pipes.shift();
+            }
+        }
+
+        // bird - pipe collision detection
+        /**
+         * if xBird > (xPipe + pipe.widgth) means bird is ahead of that pipe.
+         * checking bird y position for topPipe, bottomPipe and frontground
+         */
+        if( xBird + bird.width >= pipes[i].x  &&  xBird <= pipes[i].x + topPipe.width && (yBird <= pipes[i].y + topPipe.height || yBird + bird.height >= pipes[i].y + constant) || yBird + bird.height >=  canvas.height - frontground.height) {
+            //location.reload(); // reload the page
+            yBird = 150 // 4temp
+        }
+        
+        // incrementing score
+        if(pipes[i].x == 5){
+            score++;
+            scoreIncrement.play();
+        }
+    }
+
+    contex.drawImage(frontground, 0, canvas.height - frontground.height);
+    
+    contex.drawImage(bird, xBird, yBird);
+    
+    yBird += gravity;
+    
+    // displaying score
+    contex.fillStyle = "#000";
+    contex.font = "20px Arial";
+    contex.fillText("Score : " + score , 10, canvas.height-20);
+    
+    // requestAnimationFrame(draw);
+}
+
+draw(); // draw animation loop
